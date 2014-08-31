4d65eb6f1c7c1e642067ea9cae6f154333548c36
Yoram de Langen <yoramdelangen@gmail.com>
2014-08-31 14:28:28 +0200

    Added and modified categories and taxonomies

---
+++ models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Category class
+ *
+ * @author Yoram de Langen <yoramdelangen@gmail.com>
+ */
+class Category extends Corcel\TermTaxonomy
+{
+    /**
+     * Used to set the post's type
+     */
+    protected $taxonomy = 'category';
+}
