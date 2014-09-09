e431504b79e1dfff10df5d39ffdffc44060eeffe
Yoram de Langen <yoramdelangen@gmail.com>
2014-09-09 16:26:16 +0200

    Expanded URL function with parent->post_name and added comments to README.md

--- src/Corcel/Post.php
+++ src/Corcel/Post.php
@@ -29,9 +29,24 @@ class Post extends Eloquent
         return $this->hasMany('Corcel\PostMeta', 'post_id');
     }
 
+    /**
+     * Return all meta fields
+     *
+     * @return Corcel\PostMetaCollection
+     */
     public function fields()
     {
         return $this->meta();
+    }
+
+    /**
+     * Get parent post of current post
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function parent()
+    {
+        return $this->hasOne('Corcel\Post', 'ID', 'post_parent');
     }
 
     /**
@@ -76,8 +91,9 @@ class Post extends Eloquent
     }
 
     /**
+     * Check if the current Post/Page has a attachment
      *
-     * @return boolean [description]
+     * @return boolean
      */
     public function hasAttachment()
     {
@@ -85,13 +101,24 @@ class Post extends Eloquent
     }
 
     /**
-     * [url description]
-     * @return [type] [description]
+     * Get URL from post/page/attachment given by 'guid'. When $clean variable
+     * is true, the returned url will look clean and slug-wise (included category if is).
+     *
+     * @return string
      */
-    public function url()
+    public function url($clean=false)
     {
-        if( $this->post_type == 'attachment')
+        if( $this->post_type == 'attachment' && $clean)
+
             return $this->meta->_wp_attached_file;
+
+        else if( in_array($this->post_type, [ 'post', 'page' ]) && $clean)
+
+            if( (int) $this->post_parent > 0 )
+                return '/'. $this->parent()->first()->post_name .'/'. $this->post_name;
+            else
+                return '/'. $this->post_name;
+
         else
             return $this->guid;
     }
