70254d562d8de7d996487dc9a739f802ea23dd48
rundef <mickael@mickael-Vostro-220-Series.(none)>
2015-08-17 16:50:21 -0400

    Author bugfix

--- src/Corcel/Post.php
+++ src/Corcel/Post.php
@@ -64,7 +64,7 @@ class Post extends Eloquent
     */
     public function author(){
 
-        return $this->belongsTo('\Corcel\User', 'ID');
+        return $this->belongsTo('Corcel\User', 'post_author');
 
     }
 
