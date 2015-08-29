b5b79b89465abdb69ab115c7f6d5efd013f4b2dc
Alexander Kammerer <alexander.kammerer@online.de>
2015-08-29 22:32:33 +0200

    Enable user to set the connection
    Enables the user to set the connection attribute to use the builtin
    connections configured in Laravel.

--- src/Corcel/BaseModel.php
+++ src/Corcel/BaseModel.php
@@ -10,5 +10,26 @@ namespace Corcel;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class BaseModel extends Eloquent {
-	protected $connection = 'mysql_wp';
+	/**
+	 * You may want to use multiple databases and reset the connection for this model.
+	 * @var String
+	 */
+	private static $dbConnection;
+
+	/**
+	 * Overwrite default constructor to set the connection attribute before booting.
+	 */
+	public function __construct() {
+		// Set the connection to the one we want to use.
+		$this->setConnection(self::$dbConnection);
+		parent::__construct();
+	}
+
+	/**
+	 * Set the conection to the Wordpress database.
+	 * @param String $connection Connection used to connect to the Wordpress database.
+	 */
+	public static function setConfiguredConnection($connection) {
+		self::$dbConnection = $connection;
+	}
 }
