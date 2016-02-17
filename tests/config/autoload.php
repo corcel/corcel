8ca9bd732c8a0e3303a79c07c5366f6f997d6be2
Junior Grossi <juniorgro@gmail.com>
2016-02-17 21:27:31 -0200

    Tests using new database connection

--- tests/config/autoload.php
+++ tests/config/autoload.php
@@ -1,10 +1,22 @@
-<?php
+<?php require __DIR__ . '/../../vendor/autoload.php';
 
-require __DIR__ . '/../../vendor/autoload.php';
+use Corcel\Database\Connection;
 
-\Corcel\Database::connect(array(
-    'database'  => 'corcel-dev',
-    'username'  => 'root',
-    'password'  => '123456',
+
+//\Corcel\Database::connect(array(
+//    'database'  => 'corcel-dev',
+//    'username'  => 'homestead',
+//    'password'  => 'secret',
+//    'host' => '127.0.0.1',
+//));
+
+$params = [
+    'database' => 'corcel-dev',
+    'username' => 'homestead',
+    'password' => 'secret',
     'host' => '127.0.0.1',
-));
+];
+
+
+$corcel = new Connection('corcel');
+$corcel->add($params);
