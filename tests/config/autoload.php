862ce3504427de8c5c69f40caa56c6727b6e5fc4
Junior Grossi <juniorgro@gmail.com>
2016-02-29 22:27:29 -0300

    Running tests the old way

--- tests/config/autoload.php
+++ tests/config/autoload.php
@@ -1,17 +1,17 @@
 <?php require __DIR__ . '/../../vendor/autoload.php';
 
-//\Corcel\Database::connect(array(
-//    'database'  => 'corcel-dev',
-//    'username'  => 'homestead',
-//    'password'  => 'secret',
-//    'host' => '127.0.0.1',
-//));
+\Corcel\Database::connect(array(
+   'database'  => 'corcel-dev',
+   'username'  => 'homestead',
+   'password'  => 'secret',
+   'host' => '127.0.0.1',
+));
 
-$corcel = new \Corcel\Database\Manager();
+// $corcel = new \Corcel\Database\Manager();
 
-$corcel->addConnection([
-    'database' => 'corcel-dev',
-    'username' => 'homestead',
-    'password' => 'secret',
-    'host' => '127.0.0.1',
-], 'corcel');
+// $corcel->addConnection([
+//     'database' => 'corcel-dev',
+//     'username' => 'homestead',
+//     'password' => 'secret',
+//     'host' => '127.0.0.1',
+// ], 'corcel');
