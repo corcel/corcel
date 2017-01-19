bfbabdb70f7fc412c55683d075e27b38b7456568
Junior Grossi <juniorgro@gmail.com>
2017-01-19 19:15:12 -0200

    Add tag to service provider

--- src/Laravel/CorcelServiceProvider.php
+++ src/Laravel/CorcelServiceProvider.php
@@ -19,7 +19,7 @@ class CorcelServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/config.php' => config_path('corcel.php'),
-        ]);
+        ], 'config');
     }
     
     /**
