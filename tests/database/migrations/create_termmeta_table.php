fcca36a5ba8d9fe23bc894f9ff8543ddc4f4922c
Junior Grossi <juniorgro@gmail.com>
2017-06-14 15:31:07 -0300

    Add migration file for 'termmeta' table

---
+++ tests/database/migrations/create_termmeta_table.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+
+$capsule->schema()->create('termmeta', function (Blueprint $table) {
+    $table->increments('meta_id');
+    $table->bigInteger('term_id')->unsigned();
+    $table->string('meta_key');
+    $table->longText('meta_value');
+});
