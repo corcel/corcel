31583490188fc5652c431c6e50e95628c301d7ec
Junior Grossi <juniorgro@gmail.com>
2017-06-16 18:22:32 -0300

    Fix Term tests

--- tests/unit/TermTest.php
+++ tests/unit/TermTest.php
@@ -29,13 +29,28 @@ class TermTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
+    public function term_can_create_meta_using_helper_method()
+    {
+        $term = factory(Term::class)->create();
+
+        $term->saveMeta('foo', 'bar');
+        $meta = $term->meta;
+
+        $this->assertNotEmpty($term->meta);
+        $this->assertTrue($meta->count() > 0);
+        $this->assertEquals('bar', $term->meta->foo);
+    }
+
+    /**
+     * @test
+     */
     public function term_has_meta_associated()
     {
-        $term = Term::find(2);
+        $term = $this->createTermWithTwoMetaFields();
 
         $count = $term->meta->count();
 
-        $this->assertGreaterThan(0, $count);
+        $this->assertEquals(2, $count);
     }
 
     /**
@@ -43,10 +58,23 @@ class TermTest extends PHPUnit_Framework_TestCase
      */
     public function term_meta_can_be_queried_by_its_relation()
     {
-        $term = Term::find(2);
+        $term = $this->createTermWithTwoMetaFields();
 
         $meta = $term->meta()->where('meta_key', 'foo')->first();
 
         $this->assertEquals('bar', $meta->meta_value);
+    }
+
+    /**
+     * @return Term
+     */
+    private function createTermWithTwoMetaFields()
+    {
+        $term = factory(Term::class)->create();
+
+        $term->saveMeta('foo', 'bar');
+        $term->saveMeta('fee', 'baz');
+
+        return $term;
     }
 }
