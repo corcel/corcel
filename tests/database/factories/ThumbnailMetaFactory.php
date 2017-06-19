9bf17371a8aa97b947c67ecabbcea5f6d56fc74e
Junior Grossi <juniorgro@gmail.com>
2017-06-19 12:00:51 -0300

    Fix key on ThumbnailMeta factory

--- tests/database/factories/ThumbnailMetaFactory.php
+++ tests/database/factories/ThumbnailMetaFactory.php
@@ -4,7 +4,7 @@ use Corcel\Post;
 
 $factory->define(Corcel\ThumbnailMeta::class, function (Faker\Generator $faker) {
     return [
-        'meta_key' => $faker->word,
+        'meta_key' => '_thumbnail_id',
         'meta_value' => $faker->sentence(),
         'post_id' => function () {
             return factory(Post::class)->create()->ID;
