d0851af05b8ad15ccba7ae06e24cf0948e5d1e04
Junior Grossi <juniorgro@gmail.com>
2017-06-21 11:28:58 -0300

    Fix typo

--- tests/database/factories/OptionFactory.php
+++ tests/database/factories/OptionFactory.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Str;
 $factory->define(Corcel\Option::class, function (Faker\Generator $faker) {
     return [
         'option_name' => $faker->word,
-        'option_value' => $faker->sentence()
+        'option_value' => $faker->sentence(),
         'autoload' => 'yes',
     ];
 });
