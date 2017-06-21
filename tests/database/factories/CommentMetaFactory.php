5adffab92a5adc2b0600b8e2b2505fc19cd5ff28
Junior Grossi <juniorgro@gmail.com>
2017-06-21 11:02:26 -0300

    Add migration and factory for comment meta table

---
+++ tests/database/factories/CommentMetaFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+$factory->define(Corcel\CommentMeta::class, function (Faker\Generator $faker) {
+    return [
+        'comment_id' => $faker->numberBetween(1, 100),
+        'meta_key' => $faker->word,
+        'meta_value' => $faker->sentence(),
+    ];
+});
