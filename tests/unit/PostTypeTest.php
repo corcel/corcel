50d86d34e66ef8a7ae9972dc9ec46d14e94ead8e
Junior Grossi <juniorgro@gmail.com>
2017-07-05 09:57:02 -0300

    Fix tests for custom post type

--- tests/unit/PostTypeTest.php
+++ tests/unit/PostTypeTest.php
@@ -12,7 +12,7 @@ class PostTypeTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function custom_post_still_has_post_type()
+    public function it_still_has_post_type()
     {
         /** @var Post $post */
         $post = factory(Post::class)->create([
@@ -25,17 +25,15 @@ class PostTypeTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function custom_post_has_custom_instance_name()
+    public function it_has_custom_instance_name()
     {
         Post::registerPostType('video', Video::class);
 
         $post = factory(Post::class)->create(['post_type' => 'video']);
-        /** @var Post $post */
-        $post = Post::find($post->ID);
-        dd($post->toArray()); // FIXME
+        $post = $post->fresh();
 
         $this->assertInstanceOf(Video::class, $post);
-        $this->assertEquals('video', $post->getPostType()); // FIX
+        $this->assertEquals('video', $post->getPostType());
     }
 }
 
@@ -44,4 +42,5 @@ class PostTypeTest extends PHPUnit_Framework_TestCase
  */
 class Video extends Post
 {
+    protected $postType = 'video';
 }
