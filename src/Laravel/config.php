c393cc0ae22348fae25fa5c9c86da077eee0e9ce
Junior Grossi <juniorgro@gmail.com>
2017-07-07 11:39:34 -0300

    Add config options to be developed later

--- src/Laravel/config.php
+++ src/Laravel/config.php
@@ -5,4 +5,11 @@ return [
     // Set the connection name you'd like to use with Corcel models and classes
     'connection' => 'corcel',
 
+    'post_types' => [
+//        'video' => \App\Video::class,
+    ],
+
+    'shortcodes' => [
+//        'foo' => \App\WordPress\Shortcodes\FooShortcode::class,
+    ],
 ];
