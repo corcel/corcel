43ab94419fd9f111056c588a576daecbea396afd
Junior Grossi <juniorgro@gmail.com>
2017-10-13 15:58:31 -0300

    Register a new routes file for the Corcel api endpoints

--- src/Laravel/CorcelServiceProvider.php
+++ src/Laravel/CorcelServiceProvider.php
@@ -22,6 +22,7 @@ class CorcelServiceProvider extends ServiceProvider
     {
         $this->publishConfigFile();
         $this->registerAuthProvider();
+        $this->registerRoutes();
     }
     
     /**
@@ -49,6 +50,21 @@ class CorcelServiceProvider extends ServiceProvider
     {
         Auth::provider('corcel', function ($app, array $config) {
             return new AuthUserProvider($config);
+        });
+    }
+
+    /**
+     * @return void
+     */
+    private function registerRoutes()
+    {
+        $router = $this->app->make('router');
+
+        $router->group([
+            'namespace' => 'Corcel\Laravel\Controllers',
+            'prefix' => 'api',
+        ], function ($router) {
+            require __DIR__ . '/routes.php';
         });
     }
 }
