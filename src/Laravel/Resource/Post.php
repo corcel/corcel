aeb0fb3cd38f79477e9c99d0350c47d6ccc1025d
Junior Grossi <juniorgro@gmail.com>
2017-10-13 16:25:55 -0300

    Add more fields to the Post resource

--- src/Laravel/Resource/Post.php
+++ src/Laravel/Resource/Post.php
@@ -20,7 +20,18 @@ class Post extends Resource
     {
         return [
             'id' => $this->resource->ID,
+            'type' => $this->resource->post_type,
             'title' => $this->resource->post_title,
+            'slug' => $this->resource->post_name,
+            'excerpt' => $this->resource->post_excerpt,
+            'content' => $this->resource->post_content,
+            'status' => $this->resource->post_status,
+            'url' => $this->resource->guid,
+            'comment_status' => $this->resource->comment_status,
+            'ping_status' => $this->resource->ping_status,
+            'password' => $this->resource->post_password,
+            'created_at' => $this->resource->post_date,
+            'updated_at' => $this->resource->post_modified,
         ];
     }
 }
