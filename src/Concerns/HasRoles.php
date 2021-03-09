fc244d3a68f31eb3172b3a4a82301d88e26b25ea
A.O <35069360+oulfr@users.noreply.github.com>
2021-03-09 11:24:03 +0100

    Add users-->roles
    first version of user roles trait

---
+++ src/Concerns/HasRoles.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Concerns;
+
+use Corcel\Concerns\MetaFields;
+
+/**
+ * Trait HasRoles
+ * @package App\Concerns
+ */
+trait HasRoles
+{
+    use MetaFields;
+
+    /**
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole($role = '')
+    {
+        return in_array($role, $this->capabilities);
+    }
+
+    /**
+     * @param array $roles
+     * @return bool
+     */
+    public function hasAnyRoles($roles = [])
+    {
+        if (!empty($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCapabilitiesAttribute()
+    {
+        return array_keys($this->getMeta('wp_capabilities'));
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsAdminAttribute()
+    {
+        return $this->hasRole('administrator');
+    }
+}
